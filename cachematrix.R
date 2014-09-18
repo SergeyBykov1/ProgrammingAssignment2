ddcbebb50a805229626976e79b9d28f0981ba92e
Sergey Bykov <sergeybykov1@gmail.com>
2014-09-19 01:14:06 +0300

    Added comments to the code; Added assertions to matrix setters.

--- cachematrix.R
+++ cachematrix.R
@@ -1,37 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+##
+## Programming Assignment 2: Lexical Scoping
+## Assignment: Caching the Inverse of a Matrix
+## cachematrix.R - Simple matrix wrapper with inverse matrix caching
+##
 
-## Write a short comment describing this function
+# stopifnot acts as assertion
 
-makeCacheMatrix <- function(x = matrix()) {
-    x_inv <- NULL
+## Creates a special "matrix" object that can cache its inverse
+makeCacheMatrix <- function(mat.x = matrix()) {
+    # init inverse matrix holder
+    mat.x.inv <- NULL
     
-    set <- function(in_x){
-        x <<- in_x
-        x_inv <<- NULL
+    # original matrix setter
+    set <- function(y.in){      
+        dims <- dim(y.in)       # check if matrix is invertible
+        stopifnot(dims[1]==dims[2])
+        
+        mat.x <<- y.in
+        mat.x.inv <<- NULL
     }
-    get <- function() x 
-    setinv <- function(in_inv){
-        x_inv <<- in_inv
+    # original matrix getter
+    get <- function() mat.x
+    # inverse matrix setter
+    setinv <- function(y.inv){
+        stopifnot(dim(mat.x) == dim(y.inv))
+        mat.id <- diag(dim(mat.x)[1]) # create identity matrix
+        # check if A*inv(A)=I (identity matrix)
+        stopifnot( (mat.x %*% y.inv - mat.id) < 0.001 )
+        
+        mat.x.inv <<- y.inv # set if passed all checks
     }
-    getinv <- function() x_inv
+    # inverse matrix getter
+    getinv <- function() mat.x.inv  
     
-    # Return
-    list(set = set, get = get, setinv = setinv, getinv = getinv)
+    # Return matrix wrapper
+    list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
 
-## Write a short comment describing this function
-
+## Computes the inverse of the special "matrix" returned by 
+## makeCacheMatrix above. If the inverse has already been calculated 
+## (and the matrix has not changed), then the cachesolve should 
+## retrieve the inverse from the cache.
 cacheSolve <- function(x) {
     ## Return a matrix that is the inverse of 'x'
-    x_inv <- x$getinv()
-    if(!is.null(x_inv)) {
-        message("getting cached data")
-        return(x_inv)
+    x.inv <- x$getinv()
+    if(!is.null(x.inv)) {
+        message("Getting cached data")
+        return(x.inv)
     }
-    data <- x$get()
-    x_inv <- solve(data)
-    x$setinv(x_inv)
-    x_inv
+    x.inv <- solve(x$get())
+    x$setinv(x.inv)
+    x.inv
 }
